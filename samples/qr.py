39ad4cbc130aefe40dbff61542b8587f67e9e199
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-11-10 23:29:03 +0100

    changed qr code sample

--- samples/qr.py
+++ samples/qr.py
@@ -6,13 +6,9 @@ import backpipe
 server = backpipe.BackPipe()
 
 @server.any
-def wrong_method(r: backpipe.Request):
-    return (405, "Method not supported, please use POST.")
-
-@server.post
 def answer(r: backpipe.Request):
     try:
-        txt = r.headers["text"]
+        txt = r.path[1:]
         response = qrcode.make(txt, image_factory=qrcode.image.svg.SvgImage).to_string()
     except KeyError:
         return (400, "text header is required.")
