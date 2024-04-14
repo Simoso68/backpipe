52e43b81223002fe097675d3e597f955bbd4a60b
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2024-04-14 20:10:32 +0200

    added HTML.addMeta

--- backpipe/tools/html/__init__.py
+++ backpipe/tools/html/__init__.py
@@ -1,4 +1,5 @@
 from backpipe.tools.html.addTag import addTag as _addTag
+from backpipe.tools.html.addMeta import addMeta as _addMeta
 
 class HTML():
     """
@@ -6,10 +7,11 @@ class HTML():
     """
     def __init__(self):
         self.__data__ = "<DOCTYPE html>"
-        self.__meta__ = ""
         self.__head__ = ""
         self.__body__ = ""
     def text(self) -> str:
-        return f"{self.__data__}{self.__meta__}<html><head>{self.__head__}</head><body>{self.__body__}</body></html>"
+        return f"{self.__data__}<html><head>{self.__head__}</head><body>{self.__body__}</body></html>"
     def addTag(self, tag: str, inner: str, params: dict = {}):
         self.__body__ += _addTag(tag, inner, params)
+    def addMeta(self, name, content):
+        self.__head__ += _addMeta(name, content)
